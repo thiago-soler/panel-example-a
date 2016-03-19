7621ef6d09ff454047f968e661f7d2545639d162
thiago-soler <thiagosoler@yahoo.com.br>
2016-03-19 15:40:47 -0300

    Criando estrutura com flexbox para suporte de painel responsivo e utilizando o article com suporte a iframe.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -108,7 +108,7 @@ gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
     '.tmp/fonts/**/*'
   ]).on('change', reload);
 
-  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/styles/**/**/*.scss', ['styles']);
   gulp.watch('app/scripts/**/*.js', ['scripts']);
   gulp.watch('app/fonts/**/*', ['fonts']);
   gulp.watch('bower.json', ['wiredep', 'fonts']);
